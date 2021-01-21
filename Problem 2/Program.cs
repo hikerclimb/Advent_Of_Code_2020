7438cd2c06594abcaa10a990a530ab60085cc45d
Aniket Karmarkar <akarmarkar@phillipcapital.com>
2021-01-21 09:23:02 -0600

    finished problem 2 better way to do it is recursively

---
+++ Problem 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day1Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Aniket\Documents\Advent Of Code 2020\Day1Problem2\input.txt");
+            var list = new List<int>();
+            foreach (string line in lines)
+            {
+                list.Add(Convert.ToInt32(line));
+            }
+
+            int n = list.Count;
+            bool flip = false;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        if (list[i] + list[j] + list[k] == 2020)
+                        {
+                            Console.WriteLine(list[i] * list[j] * list[k]);
+                            flip = true;
+                            break;
+                        }
+                    }
+                }
+                if (flip == true)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
